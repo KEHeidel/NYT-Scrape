fe5d910045579f6a0aa969f213dbc2a02d74c427
Kortnie Heidel <kortnie.evans@gmail.com>
2020-06-15 02:08:12 -0400

    creates routes.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 var PORT = process.env.PORT || 3000;
 
@@ -6,9 +9,31 @@ var app = express();
 
 var router = express.Router();
 
+require("./config/routes")(router);
+
 app.use(express.static(__dirname + "/public"));
 
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(db, function(error) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
 
 app.listen(PORT, function() {
     console.log("Listening on port:" + PORT);
